41b82afb3eb005c5c660562868521f932d6490a2
Ricky Ryklief <rykliefr@gmail.com>
2026-05-17 11:53:30 +0100

    feat: update sanity settings

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client';
 export const sanityClient = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  useCdn: true, // Uses edge caching for blazing fast loads
-  apiVersion: '2026-05-15', // Today's date to lock in the API structure
+  useCdn: false, // Uses edge caching for blazing fast loads
+  apiVersion: '2026-05-17', // Today's date to lock in the API structure
 });
